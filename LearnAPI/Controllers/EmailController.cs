a7bbe9309cb75b1e7efd43ace6d2673062ad18c1
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2023-10-04 12:28:44 +0530

    Mail with request body params complete

--- LearnAPI/Controllers/EmailController.cs
+++ LearnAPI/Controllers/EmailController.cs
@@ -16,17 +16,18 @@ namespace LearnAPI.Controllers
             this.emailService = emailService;
         }
 
-
-        [HttpGet("SendMail")]
-
-        public async Task<IActionResult> Sendmail()
+        //[HttpGet("SendMail")]
+        [HttpPost("SendMail")]
+        public async Task<IActionResult> Sendmail(MailRequest mail)
         {
             try
             {
                 MailRequest mailRequest = new MailRequest();
-                mailRequest.ToEmail = "prajapatisanjiv8@gmail.com";
-                mailRequest.Subject = "test";
-                mailRequest.Body = GetHtmlContent();
+            /*  mailRequest.ToEmail = "prajapatisanjiv8@gmail.com";
+                mailRequest.Subject = "test"; */
+                mailRequest.ToEmail = mail.ToEmail;
+                mailRequest.Subject = mail.Subject;
+                mailRequest.Body = GetHtmlContent(mail.Body);
                 await emailService.SendEmailAsync(mailRequest);
                 return Ok(mailRequest);
             }
@@ -37,9 +38,10 @@ namespace LearnAPI.Controllers
            
         }
 
-        private string GetHtmlContent()
+        private string GetHtmlContent(string data)
         {
             string response = "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n<head>\r\n    <meta charset=\"UTF-8\">\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n    <title>Document</title>\r\n    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM\" crossorigin=\"anonymous\">\r\n</head>\r\n<body>\r\n    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz\" crossorigin=\"anonymous\"></script>\r\n\r\n\r\n    <div class=\"container\">\r\n        <div class=\"card\" style=\"width: auto;border: none;background-color: rgb(160, 252, 252);\">\r\n            <div class=\"card-header\">\r\n                <h2 style=\"text-align: center;\">Welcome to the Blog!</h2><br/>\r\n                <h2 style=\"color: rgb(16, 64, 16);\">Hi</h2>\r\n            </div>\r\n            <div class=\"cart-body\" style=\"margin: 10px;\">\r\n              <h3>this is dummy mail formate.</h3><br>\r\n\r\n              <h4>Contact Us : prajapatisanjiv8@gmail.com</h4>\r\n\r\n            </div>\r\n\r\n             \r\n        </div>\r\n    </div>\r\n\r\n\r\n</body>\r\n</html>";
+            response += data;
             return response;
         }
     }
